9a895778c4801aec6dd8a3a7305b5e89eacb5f9b
François Grabenstaetter <francoisgrabenstaetter@gmail.com>
2019-04-07 19:43:32 +0200

    update

--- ui/env.py
+++ ui/env.py
@@ -1,6 +1,6 @@
 DRAWING_AREA_SIZE = 340
 VERSION = '0.2'
 PROGRAM_NAME = 'Reconnaissance de caractère'
-CHARACTERS_LIST = ['A', 'B', 'C', 'D', 'E', 'F', 'G',' H', 'I',' J', 'K', 'L',
+CHARACTERS_LIST = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',' J', 'K', 'L',
                    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',' W',' X',
                    'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
