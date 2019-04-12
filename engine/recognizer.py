8c1ef511bbd461884874db41789618073c964459
François Grabenstaetter <francoisgrabenstaetter@gmail.com>
2019-04-12 13:27:32 +0200

    update

--- engine/recognizer.py
+++ engine/recognizer.py
@@ -27,7 +27,7 @@ class Recognizer ():
     def clean_extra_positions (self):
         """ Remove every position which is too closer with another """
         new_positions = []
-        min_diff = DRAWING_AREA_SIZE // 100
+        min_dist = DRAWING_AREA_SIZE // 40
         last_pos = None, None
 
         for key, (x, y) in enumerate(self.positions):
@@ -35,16 +35,17 @@ class Recognizer ():
                 continue
             none_neighbour = (last_pos == (None, None)) or \
                              (self.positions[key + 1] == (None, None))
-            diff_ok = (not none_neighbour) and \
-                      (math.fabs(x - last_pos[0]) >= min_diff) and \
-                      (math.fabs(y - last_pos[1]) >= min_diff)
-            if (none_neighbour or diff_ok):
+            dist_ok = (not none_neighbour) and \
+                      (math.sqrt(math.pow(x - last_pos[0], 2) + \
+                                 math.pow(y - last_pos[1], 2)) >= min_dist)
+            if (none_neighbour or dist_ok):
                 last_pos = x, y
                 new_positions.append(last_pos)
         self.positions = new_positions
 
     def stretch_positions (self):
-        """ Change the coords to scale them in a 100x100 area """
+        """ Change the coords to scale them (adjust and center) in a 100x100
+            area """
         new_size = 100
         min_pos_x, max_pos_x, min_pos_y, max_pos_y = None, None, None, None
         for x, y in self.positions:
@@ -66,36 +67,54 @@ class Recognizer ():
             try:
                 x_relative_pos = (x - left_diff) / \
                                  (DRAWING_AREA_SIZE - left_diff - right_diff)
-            except ArithmeticError:
+            except ArithmeticError: # zero division error
                 x_relative_pos = 1
-            new_x = math.floor((x + right_diff * x_relative_pos - \
-                                left_diff * (1 - x_relative_pos)) * \
-                                (new_size / DRAWING_AREA_SIZE))
             try:
                 y_relative_pos = (y - top_diff) / \
                                  (DRAWING_AREA_SIZE - top_diff - bottom_diff)
-            except ArithmeticError:
+            except ArithmeticError: # zero division error
                 y_relative_pos = 1
+
+            new_x = math.floor((x + right_diff * x_relative_pos - \
+                                left_diff * (1 - x_relative_pos)) * \
+                                (new_size / DRAWING_AREA_SIZE))
             new_y = math.floor((y + bottom_diff * y_relative_pos - top_diff *
                                (1 - y_relative_pos)) * \
                                (new_size / DRAWING_AREA_SIZE))
             self.positions[key] = new_x, new_y
+        print('\n@@@ STRETCH POSITIONS @@@\n')
+        self.print_draw(100)
 
     def adjust_positions_number (self):
-        """ Add or remove some positions to have exactly 20 positions (to
+        """ Add or remove some positions to have exactly 40 positions (to
             compare them easier) """
-        number = 20
+        number = 60
         size = len(self.positions)
         diff = int(math.fabs(size - number))
+        incr = size / diff
+        k = incr
         if (diff > 0):
-            k = size // diff - 1
             for i in range(0, diff):
+                q = math.floor(k)
+                if q < 1:
+                    q = 1
+                elif q >= size:
+                    q = size - 1
+
                 if (size < number):
-                    self.positions.insert(k, self.positions[k])
+                    xMoy = (self.positions[q-1][0] + self.positions[q][0]) // 2
+                    yMoy = (self.positions[q-1][1] + self.positions[q][1]) // 2
+                    self.positions.insert(q, (xMoy, yMoy))
+                    size += 1
+                    k += 1
                 elif (size > number):
-                    self.positions.remove(self.positions[k])
-                k += size // diff - 1
+                    self.positions.remove(self.positions[q])
+                    size -= 1
+                    k -= 1
+                k += incr
             assert len(self.positions) == number
+        print('\n@@@ ADJUST POSITIONS NUMBER @@@\n')
+        self.print_draw(100)
 
     def best_match (self):
         """ Return the most probable character (with comparison of
@@ -131,3 +150,13 @@ class Recognizer ():
             if (char.positions is not None):
                 char.delete_positions()
                 char.positions = None
+
+    def print_draw (self, max):
+        for j in range(max):
+            print('\n|', end = '')
+            for i in range(max):
+                if ((i, j) in self.positions):
+                    print('*', end = '')
+                else:
+                    print(' ', end = '')
+            print('|', end = '')
